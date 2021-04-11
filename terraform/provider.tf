8d7c5e16879da318bf20c604b3255ca625f06998
ashishkarpe <ashishkarpe@gmail.com>
2021-04-11 10:54:01 +0000

    Created EKS cluster sucessfully

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,16 @@
 provider "aws" {
-  access_key = "AKIAQF6YGRERJZYSWPM3"
-  secret_key = "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"
+//  access_key = "AKIAQF6YGRERJZYSWPM3"
+//  secret_key = "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"
+
+
+  access_key = "AKIAQF6YGRERJDJJRKT5"
+  secret_key = "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"
   region     = var.AWS_REGION
 }
+
+data "aws_availability_zones" "available" {
+}
+
+provider "http" {
+}
+
