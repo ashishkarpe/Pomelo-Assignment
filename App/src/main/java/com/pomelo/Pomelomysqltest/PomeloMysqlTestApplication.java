d499aba59254a4f9f962e894b3bad3d314ee2f56
Ashish Karpe <ashishkarpe@gmail.com>
2021-04-17 17:54:02 +0700

    Update PomeloMysqlTestApplication.java

--- App/src/main/java/com/pomelo/Pomelomysqltest/PomeloMysqlTestApplication.java
+++ App/src/main/java/com/pomelo/Pomelomysqltest/PomeloMysqlTestApplication.java
@@ -2,17 +2,17 @@ package com.pomelo.Pomelomysqltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class PomeloMysqlTestApplication {
 
-	/* @RequestMapping("/")
+  @RequestMapping("/")
   public String home() {
     return "Hello Docker World";
-  }*/
+  }
 	public static void main(String[] args) {
 		SpringApplication.run(PomeloMysqlTestApplication.class, args);
 	}
