ec2aea5ca86ff7f7cda839a6d88417acf11d0fc7
ashishkarpe <ashishkarpe@gmail.com>
2021-04-18 13:37:00 +0000

    LB created

--- terraform/eks-workers.tf
+++ terraform/eks-workers.tf
@@ -37,10 +37,10 @@ resource "aws_launch_configuration" "demo" {
 }
 
 resource "aws_autoscaling_group" "demo" {
-  desired_capacity = 0
+  desired_capacity = 1
   launch_configuration = aws_launch_configuration.demo.id
-  max_size = 0
-  min_size = 0
+  max_size = 1
+  min_size = 1
   name = "terraform-eks-demo"
   # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
   # force an interpolation expression to be interpreted as a list by wrapping it
