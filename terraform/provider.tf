f678521a5827946c7cff9a948a5074aecad814f5
ashishkarpe <ashishkarpe@gmail.com>
2021-04-19 09:54:12 +0000

    updated code for aws keys

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,6 @@
 provider "aws" {
-//  access_key = "AKIAQF6YGRERJZYSWPM3"
-//  secret_key = "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"
 
 
-  access_key = "AKIAQF6YGRERJDJJRKT5"
-  secret_key = "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"
   region     = var.AWS_REGION
 }
 
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,15 +2,13 @@ variable "cluster-name" {
   default = "terraform-eks-Pomelo-Assignment"
   type    = string
 }
-/*
+
 variable "AWS_ACCESS_KEY" {
-  default = "AKIAQF6YGRERJZYSWPM3" 
 }
 
 variable "AWS_SECRET_KEY" {
-  default = "EnRcQtdYM6M4Tr3KuNUkaXe9Gx8XVmw7mCvTe/8c"
 }
-*/
+
 variable "AWS_REGION" {
   default = "us-east-1"
 }
